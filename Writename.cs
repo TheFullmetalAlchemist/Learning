a6881af6fc1259cdacd2cca85b0d2d5ee265f6ad
vijay <vj366331@gmail.com>
2024-02-14 09:45:45 +0530

    updated

--- Writename.cs
+++ Writename.cs
@@ -77,8 +77,24 @@ public void Connie(){
     var numbers= new List<int>{1,2,3,4};
     numbers.Add(1);
     numbers.AddRange(new int[3]{5,6,7});
-    foreach(var n in numbers){
+    foreach(var n in numbers)
         System.Console.WriteLine(n);
+    System.Console.WriteLine();
+    System.Console.WriteLine(numbers.IndexOf(5));   
+    System.Console.WriteLine("count is"+numbers.Count);
+    numbers.Remove(1);
+    foreach(var n in numbers)
+        System.Console.WriteLine("removing start of 1: "+n);
+
+    System.Console.WriteLine("count is"+numbers.Count);
+    for (var i=0; i<numbers.Count;i++){
+        if(numbers[i]==1)
+            numbers.Remove(numbers[i]);
+        }
+        foreach(var n in numbers)
+        System.Console.WriteLine(n);
+
+        numbers.Clear();
+    System.Console.WriteLine(numbers.Count);
     }
-}
 }
