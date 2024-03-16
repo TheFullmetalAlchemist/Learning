367cafc4a30f38c9880ef34d02544a61d4999672
vijay <vj366331@gmail.com>
2024-03-16 12:14:56 +0530

    final

--- Program.cs
+++ Program.cs
@@ -2,15 +2,37 @@ using System;
 using System.Collections;
 using System.Globalization;
 using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.VisualBasic;
 namespace WebApplication1;
 
-
 public class Program
 {
-  public static void Main(string[] args){
-    var result = new Writename();
-       result.Reiner();
-    }    
+  public static void Main(){
+    // System.Console.WriteLine("Enter a number:");
+    // var a = Convert.ToInt16(Console.ReadLine());
+    // System.Console.WriteLine("Enter another number:");
+    // var b = Convert.ToInt16(Console.ReadLine());
+    
+        Console.Write("Input a number(integer): ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int sum = SumOfDigits(number);
+
+        Console.WriteLine("Sum of the digits of the said integer: " + sum);
+    }
+
+    // Function to compute the sum of the digits of an integer
+    public static int SumOfDigits(int num){
+        int sum= 0;
+        while(num!=0){
+          sum+=num%10;
+          num/=10;
+          
+        }return sum;
+    }
 }
+
+
+
 
--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d44b96fb5082069c835f93dce6d215802602ed8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
